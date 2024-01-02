21a7ca9089a04b15d82414acb30540abec30c653
briggsb9 <ben_briggs@outlook.com>
2024-01-02 23:32:57 +0000

    example update

--- solarProductionAdvisor/example-advisorConfig.py
+++ solarProductionAdvisor/example-advisorConfig.py
@@ -9,6 +9,8 @@ logfile = './solar-production-advisor-log.log'
 # Variables for Solar production forecasting. Currently using: https://doc.forecast.solar/api:estimate 
 solar_forecast_api_key = '' 
 solar_forecast_endpoint = 'https://api.forecast.solar/estimate/watts/LAT/LONG/AZIMUTH/TILT/CAPACITY' # (lat, long, azimuth, tilt, capacity)
+use_test_data = False # Use test data from test.json instead of API
+test_json_path = './test.json'
 
 # Variables for the Telegram bot
 token = 'YOURTOKEN'
